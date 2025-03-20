824e8e6ca7d8af29403b5e0f6cac3839a8cc776b
dirkarnez <smalldirkalex@gmail.com>
2025-03-20 18:05:52 +0800

    - wip

--- app/local-composer-upgrade.cmd
+++ app/local-composer-upgrade.cmd
@@ -18,11 +18,14 @@ set PATH=^
 @REM %LARAGON_DIR%\bin\php\php-8.4.0-Win32-vs17-x64\php.exe "%LARAGON_DIR%\bin\composer\composer.phar" require symfony/expression-language -vvv
 @REM cd app &&^
 @REM composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
-%LARAGON_DIR%\bin\php\php-8.4.0-Win32-vs17-x64\php.exe -r "echo ini_get('memory_limit').PHP_EOL;"
-%LARAGON_DIR%\bin\php\php-8.4.0-Win32-vs17-x64\php.exe "%LARAGON_DIR%\bin\composer\composer.phar" init -vvv
-%LARAGON_DIR%\bin\php\php-8.4.0-Win32-vs17-x64\php.exe "%LARAGON_DIR%\bin\composer\composer.phar" install -vvv
-%LARAGON_DIR%\bin\php\php-8.4.0-Win32-vs17-x64\php.exe "%LARAGON_DIR%\bin\composer\composer.phar" upgrade -vvv
-%LARAGON_DIR%\bin\php\php-8.4.0-Win32-vs17-x64\php.exe "%LARAGON_DIR%\bin\composer\composer.phar" update -vvv
+
+
+
+@REM %LARAGON_DIR%\bin\php\php-8.4.0-Win32-vs17-x64\php.exe -r "echo ini_get('memory_limit').PHP_EOL;"
+@REM %LARAGON_DIR%\bin\php\php-8.4.0-Win32-vs17-x64\php.exe "%LARAGON_DIR%\bin\composer\composer.phar" init -vvv
+@REM %LARAGON_DIR%\bin\php\php-8.4.0-Win32-vs17-x64\php.exe "%LARAGON_DIR%\bin\composer\composer.phar" install -vvv
+@REM %LARAGON_DIR%\bin\php\php-8.4.0-Win32-vs17-x64\php.exe "%LARAGON_DIR%\bin\composer\composer.phar" upgrade -vvv
+@REM %LARAGON_DIR%\bin\php\php-8.4.0-Win32-vs17-x64\php.exe "%LARAGON_DIR%\bin\composer\composer.phar" update -vvv
 
 @REM composer config -g repo.packagist composer https://packagist.phpcomposer.com
 @REM composer install -vvv 
--- app/local-composer-upgrade.sh
+++ app/local-composer-upgrade.sh
@@ -5,9 +5,13 @@ export LARAGON_DIR="$DOWNLOADS_DIR/laragon-php-8.4.0-mariadb-10.11.10-portable-v
 export PATH="/mingw64/bin:/usr/local/bin:/usr/bin:/bin"
 export PATH="$PATH:$LARAGON_DIR/bin/php/php-8.4.0-Win32-vs17-x64:$LARAGON_DIR/bin/composer:$DOWNLOADS_DIR/PortableGit/bin"
 
-php -r "ini_set('memory_limit', '2G');echo ini_get('memory_limit').PHP_EOL;"
-composer config -g repo.packagist composer https://packagist.phpcomposer.com
-$LARAGON_DIR/bin/composer/composer.phar --no-dev install
+# php -r "ini_set('memory_limit', '2G');echo ini_get('memory_limit').PHP_EOL;"
+# composer config -g repo.packagist composer https://packagist.phpcomposer.com
+# $LARAGON_DIR/bin/composer/composer.phar --no-dev install
+
+
+# php composer-setup.php --install-dir=./bin
+composer install -vvv 
 
 # @REM %LARAGON_DIR%\bin\php\php-8.4.0-Win32-vs17-x64\php.exe "%LARAGON_DIR%\bin\composer\composer.phar" init -vvv &&^
 # @REM %LARAGON_DIR%\bin\php\php-8.4.0-Win32-vs17-x64\php.exe "%LARAGON_DIR%\bin\composer\composer.phar" config -g repo.packagist composer https://packagist.phpcomposer.com &&^
